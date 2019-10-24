99b66fdab2efb4868d594427030804148076d46d
Przemyslaw Korecki <Przemyslaw.Korecki@mag.pl>
2019-10-24 15:20:07 +0200

    Add menu

--- app/src/main/java/com/example/przemek4/MainActivity.kt
+++ app/src/main/java/com/example/przemek4/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.przemek4
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -38,5 +41,33 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, PiataAktywnosc::class.java))
             true}
 
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var selectedOption=""
+        when(item?.itemId){
+            R.id.menuItem2 -> startActivity(Intent(this, DrugaAktywnosc::class.java))
+            R.id.menuItem3 -> startActivity(Intent(this, TrzeciaAktywnosc::class.java))
+            R.id.menuItem4 -> startActivity(Intent(this, CzwartaAktywosc::class.java))
+            R.id.menuItem5 -> startActivity(Intent(this, PiataAktywnosc::class.java))
+            R.id.menuItem6 -> startActivity(Intent(this, SzostaAktywnosc::class.java))
+            R.id.menuItem7 -> selectedOption = "7 jeszcze nie oprogramowana opcja wyboru"
+            R.id.menuItem8 -> selectedOption = "8 jeszcze nie oprogramowana opcja wyboru"
+            R.id.menuItem9 -> selectedOption = "9 jeszcze nie oprogramowana opcja wyboru"
+            R.id.menuItem10 -> selectedOption = "10 jeszcze nie oprogramowana opcja wyboru"
+            R.id.menuItem11 -> selectedOption = "11 jeszcze nie oprogramowana opcja wyboru"
+
+        }
+        Toast.makeText(this, "Opcja "+ selectedOption, Toast.LENGTH_LONG ).show()
+        return super.onOptionsItemSelected(item)
+    }
+
 }
